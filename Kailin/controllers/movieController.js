93553a172e3e1ea921d7161ee5414f9dfb97d62f
nahkailin <74762456+nahkailin@users.noreply.github.com>
2021-01-13 18:01:40 +0800

    moviecontroller. hardcoded data for me to do my functions

---
+++ Kailin/controllers/movieController.js
@@ -0,0 +1,32 @@
+// var movieController = {
+//     movie: [
+//         {
+//             //hardcoded
+//             title: "ok",
+//             genre: "ok",
+//             language: "d",
+//             runningTime: "d",
+//             director: "f",
+//             cast: "a",
+//             description: "b"
+//         },
+//         {
+//             //hardcoded
+//             title: "oh wow",
+//             genre: "wow",
+//             language: "d",
+//             runningTime: "d",
+//             director: "f",
+//             cast: "a",
+//             description: "b"
+//         }
+//     ], 
+//     getMovie: function() {
+//         return this.movie;
+//     },
+//     addMovie: function(newMovie) {
+//         this.movie.push(newMovie);
+//     }
+// };
+
+// module.exports=movieController;
