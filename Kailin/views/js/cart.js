3df6a4b66a0afd005b860990b269dec8f77f51b0
nahkailin <74762456+nahkailin@users.noreply.github.com>
2021-01-18 19:07:48 +0800

    cart

--- Kailin/views/js/cart.js
+++ Kailin/views/js/cart.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+    userId = sessionStorage.Id
+
     $.ajax({
-        url: "/cart",
+        url: "/cart/" + userId,
         method: "get"
     })
         .done(
