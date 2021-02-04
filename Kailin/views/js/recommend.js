80e47dc44e7b9316c21ee8579cc3fce23aeeb47f
tara773 <66659165+tara773@users.noreply.github.com>
2021-02-05 01:15:21 +0800

    Add files via upload

--- Kailin/views/js/recommend.js
+++ Kailin/views/js/recommend.js
@@ -10,12 +10,13 @@ $(document).ready(function () {
                         <article>
                         <h2><a href="/edit?id=${movie._id}">${movie.title}</a></h2>
                         <div>
-                            ${movie.genre}<br>
-                            ${movie.language}<br>
-                            ${movie.runningTime}<br>
-                            ${movie.Director}<br>
-                            ${movie.Cast}<br>
-                            ${movie.description}<br>
+                            Genre: ${movie.genre}<br>
+                            Language: ${movie.language}<br>
+                            Running Time: ${movie.runningTime}<br>
+                            Director: ${movie.Director}<br>
+                            Cast: ${movie.Cast}<br>
+                            Description: ${movie.description}<br>
+                            Rating: ${movie.rating}<br>
                         </div>
                         </article>
                     `);
